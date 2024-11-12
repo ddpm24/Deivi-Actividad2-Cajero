2702d1af97b613d35ddcd6579dbc6d819bd18f5c
Deivi <deivi54357326@gmail.com>
2024-11-12 12:06:01 +0100

    Initial commit

---
+++ src/main/java/cajeroweb/DeiviActividad2CajeroApplication.java
@@ -0,0 +1,13 @@
+package cajeroweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeiviActividad2CajeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeiviActividad2CajeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cajeroweb/DeiviActividad2CajeroApplicationTests.java
@@ -0,0 +1,13 @@
+package cajeroweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeiviActividad2CajeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
